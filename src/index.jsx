ed357045c47304bfd07d5f80018ce24b3ead8e0e
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-07-25 21:49:02 +1000

    second commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -63,4 +64,8 @@ library.add(
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>
+);
